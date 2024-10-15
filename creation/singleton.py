03b40601049dd6c46b789ffa512dcb039f693287
Daniel Hurtado <81983296+DHurtado714-itesm@users.noreply.github.com>
2024-10-15 22:27:10 +0200

    feat: singleton pattern

---
+++ creation/singleton.py
@@ -0,0 +1,13 @@
+class Singleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls)
+        return cls._instance
+
+
+singleton1 = Singleton()
+singleton2 = Singleton()
+
+print(singleton1 is singleton2)  # True
